020914ae4885bfe7e6b3e8f02a0e316c6dce920c
Mike <maquinde@users.noreply.github.com>
2019-06-19 13:09:36 -0700

    created class extra-info to hide empty element with jquery

--- google-search-results/scripts/main.js
+++ google-search-results/scripts/main.js
@@ -1,1 +1,14 @@
-$("main.empty:empty").hide();
+//TEST IF JQUERY IS LOADED
+// 
+// window.onload = function() {
+//     if (window.jQuery) {  
+//         // jQuery is loaded  
+//         alert("Yeah!");
+//     } else {
+//         // jQuery is not loaded
+//         alert("Doesn't Work");
+//     }
+// }
+$(function() {
+    $(".extra-info:empty").hide();
+});
